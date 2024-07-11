99102c72de5c2c03df85c73724b4298635fbe4bb
Mohammed Azeezulla <mdazeezulla2001@gmail.com>
2024-07-11 12:54:24 -0500

    evaluation state prepared

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -39,11 +39,11 @@ class ModelTrainerConfig:
 
 
 
-# @dataclass(frozen=True)
-# class ModelEvaluationConfig:
-#     root_dir: Path
-#     test_data_path: Path
-#     model_path: Path
-#     all_params: dict
-#     metric_file_name: Path
-#     target_column: str
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
